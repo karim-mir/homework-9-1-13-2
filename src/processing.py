f65d2fb063d18067910178c49abd648546ad13ce
NazKar@Nazir <karimov.nazir00@yandex.ru>
2024-09-14 09:49:37 +0500

    Implementation of functions

--- src/processing.py
+++ src/processing.py
@@ -21,3 +21,30 @@ print(
         ]
     )
 )
+
+from datetime import datetime
+
+def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """
+    Сортирует список словарей по дате
+    :param data: Список словарей, который нужно сортировать.
+    :param reverse: Параметр для определения порядка сортировки.
+                    По умолчанию True (убывание). Если False, то сортировка в порядке возрастания.
+    :return: Новый список словарей, отсортированный по дате
+    """
+    return sorted(data,
+                  key=lambda x: datetime.fromisoformat(x['date']),
+                  reverse=reverse)
+
+
+print(
+    sort_by_date(
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+         ]
+    )
+)
+
