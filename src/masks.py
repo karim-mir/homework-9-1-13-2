9f80c1583afd55fbf47a1418e352a7fe3cfec22f
NazKar@Nazir <karimov.nazir00@yandex.ru>
2024-09-16 20:52:07 +0500

    linking repositories small fixes in README.md Add test_masks.py and fixes func in masks.py Both functions implemented and fixed

--- src/masks.py
+++ src/masks.py
@@ -11,7 +11,7 @@ def get_mask_card_number(card_number: int) -> str:
     masked_card_number_str = card_number_str[:6] + "*" * (len(card_number_str) - 10) + card_number_str[-4:]
 
     # Разделяем строку на группы по 4 символа
-    masked_card_number = [masked_card_number_str[i: i + 4] for i in range(0, len(masked_card_number_str), 4)]
+    masked_card_number = [masked_card_number_str[i : i + 4] for i in range(0, len(masked_card_number_str), 4)]
 
     # Объединяем группы с пробелами
     return " ".join(masked_card_number)
@@ -23,6 +23,11 @@ print(get_mask_card_number(7000792289606361))
 def get_mask_account(account_number: int) -> str:
     """Преобразуем номер счета в строку"""
     account_number_str = str(account_number)
+
+    # Проверяем, состоит ли номер карты только из цифр и имеет ли нужную длину
+    if not account_number_str.isdigit() or len(account_number_str) < 16 or len(account_number_str) > 20:
+        raise ValueError("Введен некорректный номер счета")
+
     # Создаем маску: первые символы заменяем на две *, и оставляем последние 4 символа
     masked_account_number = "*" * 2 + account_number_str[-4:]
     return masked_account_number
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,21 +1,24 @@
 import pytest
 from src.masks import get_mask_card_number
 
+
 @pytest.fixture
 def card_numbers():
     return [
-        '7000792289606361',   # Вводимое значение для первого теста
-        '',                   # Вводимое значение для второго теста
-        '700079228960636100', # Вводимое значение для третьего теста
+        "7000792289606361",  # Вводимое значение для первого теста
+        "",  # Вводимое значение для второго теста
+        "700079228960636100",  # Вводимое значение для третьего теста
     ]
+
 
 @pytest.fixture
 def expected_results():
     return [
-        '7000 79** **** 6361',  # Ожидаемый результат для первого теста
-        ValueError,              # Ожидаемое исключение для второго теста
-        ValueError               # Ожидаемое исключение для третьего теста
+        "7000 79** **** 6361",  # Ожидаемый результат для первого теста
+        ValueError,  # Ожидаемое исключение для второго теста
+        ValueError,  # Ожидаемое исключение для третьего теста
     ]
+
 
 def test_get_mask_card_number(card_numbers, expected_results):
     for card_number, expected in zip(card_numbers, expected_results):
@@ -24,4 +27,32 @@ def test_get_mask_card_number(card_numbers, expected_results):
                 get_mask_card_number(card_number)
         else:
             result = get_mask_card_number(card_number)
+            assert result == expected
+
+
+@pytest.fixture
+def account_numbers():
+    return [
+        "73654108430135874305",  # Вводимое значение для первого теста
+        "",  # Вводимое значение для второго теста
+        "700079228960636100",  # Вводимое значение для третьего теста
+    ]
+
+
+@pytest.fixture
+def expected_results_account():
+    return [
+        "**4305",  # Ожидаемый результат для первого теста
+        ValueError,  # Ожидаемое исключение для второго теста
+        ValueError,  # Ожидаемое исключение для третьего теста
+    ]
+
+
+def test_get_mask_account_number(account_numbers, expected_results_account):
+    for account_number, expected in zip(account_numbers, expected_results_account):
+        if expected is ValueError:
+            with pytest.raises(ValueError):
+                get_mask_account_number(account_number)
+        else:
+            result = get_mask_account_number(account_number)
             assert result == expected
