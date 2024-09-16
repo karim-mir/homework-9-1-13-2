49c35f4e710acc0c822bb2c09b196cc32817c161
NazKar@Nazir <karimov.nazir00@yandex.ru>
2024-09-16 23:41:01 +0500

    linking repositories small fixes in README.md Add test_masks.py and fixes func in masks.py Both functions implemented and fixed Fix code in widget.py and test_widget.py, add test_widget.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,13 +1,13 @@
 import pytest
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account  # Обратите внимание на правильный импорт
 
 
 @pytest.fixture
 def card_numbers():
     return [
-        "7000792289606361",  # Вводимое значение для первого теста
-        "",  # Вводимое значение для второго теста
-        "700079228960636100",  # Вводимое значение для третьего теста
+        7000792289606361,  # Вводимое значение для первого теста
+        0,  # Вводимое значение для второго теста (для проверки пустыми)
+        700079228960636100,  # Вводимое значение для третьего теста
     ]
 
 
@@ -33,16 +33,16 @@ def test_get_mask_card_number(card_numbers, expected_results):
 @pytest.fixture
 def account_numbers():
     return [
-        "73654108430135874305",  # Вводимое значение для первого теста
-        "",  # Вводимое значение для второго теста
-        "700079228960636100",  # Вводимое значение для третьего теста
+        73654108430135874305,  # Вводимое значение для первого теста
+        0,  # Вводимое значение для второго теста (для проверки пустыми)
+        700079228960636100,  # Вводимое значение для третьего теста
     ]
 
 
 @pytest.fixture
 def expected_results_account():
     return [
-        "**4305",  # Ожидаемый результат для первого теста
+        "7365 41** **** 4305",  # Ожидаемый результат для первого теста (обновлен)
         ValueError,  # Ожидаемое исключение для второго теста
         ValueError,  # Ожидаемое исключение для третьего теста
     ]
@@ -52,7 +52,7 @@ def test_get_mask_account_number(account_numbers, expected_results_account):
     for account_number, expected in zip(account_numbers, expected_results_account):
         if expected is ValueError:
             with pytest.raises(ValueError):
-                get_mask_account_number(account_number)
+                get_mask_account(account_number)
         else:
-            result = get_mask_account_number(account_number)
+            result = get_mask_account(account_number)
             assert result == expected
